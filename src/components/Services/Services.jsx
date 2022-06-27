6516c5832c7e963c8c2d5e8abf9661a9ec972d64
MatanSaban <105128031+MatanSaban@users.noreply.github.com>
2022-06-27 21:25:34 +0300

    aa

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,11 +1,48 @@
 import SinglePagesHero from "../Special/SinglePage/SinglePagesHero/SignlePagesHero";
+import Json from "../Header/MenuItems.json";
+import "./services.css";
+import { NavLink } from "react-router-dom";
 
 const Services = (props) => {
-    return ( 
+    return (
         <div id="ServicesPage" className="servicesWrapper">
-            <SinglePagesHero title={props.pageName} parentName={props.parentName}  parentPath={props.parentPath} />
+            <SinglePagesHero
+                title={props.pageName}
+                parentName={props.parentName}
+                parentPath={props.parentPath}
+            />
+            <div className="allServices">
+                {Object.keys(Json.Services.Submenu).map((keyName, index) => {
+                    return (
+                        <div className="service" style={{display:'flex', flexDirection:'column', alignItems:'flex-start', alignContent:'center'}}>
+                            <NavLink to={Json.Services.Submenu[keyName].link}><h2>{Json.Services.Submenu[keyName].text}</h2></NavLink>
+
+                            {Json.Services.Submenu[keyName].subSubmenu && (
+                                <ul style={{display:'flex', flexDirection:'column', alignItems:'flex-start', alignContent:'center', justifyContent:'center', padding:'0 5px 0 0 '}}>
+                                    {Object.keys(
+                                        Json.Services.Submenu[keyName]
+                                            .subSubmenu
+                                    ).map((keyname) => {
+                                        return (
+                                            <NavLink to={`${Json.Services.Submenu[keyName].link}${Json.Services.Submenu[
+                                                keyName
+                                            ].subSubmenu[keyname].link}`}>
+                                                {
+                                                    Json.Services.Submenu[
+                                                        keyName
+                                                    ].subSubmenu[keyname].text
+                                                }
+                                            </NavLink>
+                                        );
+                                    })}
+                                </ul>
+                            )}
+                        </div>
+                    );
+                })}
+            </div>
         </div>
-     );
-}
- 
+    );
+};
+
 export default Services;
