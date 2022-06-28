da86cc82960fd0616561029089337ec724a77562
MatanSaban <105128031+MatanSaban@users.noreply.github.com>
2022-06-28 11:53:10 +0300

    SingleBlog

--- src/components/Blog/SingleBlog.jsx
+++ src/components/Blog/SingleBlog.jsx
@@ -38,25 +38,25 @@ const SingleBlog = (props) => {
     return (
         <div id="singleBlog" className="singleBlogWrapper">
             <section className="hero">
-                <Container path={BlogJson.theBlog.articles[link].image}>
+                {/* <Container path={BlogJson.theBlog.articles[link].image}>
                     <div
                         className="singleBlog__hero"
                         style={{
                             background: `url(${BlogJson.theBlog.articles[link].image})`,
                         }}
                     ></div>
-                </Container>
+                </Container> */}
                 <SinglePagesHero
                     title={BlogJson.theBlog.articles[link].title}
                     parentName={props.parentName}
                     parentPath={props.parentPath}
                 />
-            </section>
-            <section className="blogContent">
                         <div className="heading">
                             <h2>{BlogJson.theBlog.articles[link].headingTitle}</h2>
                             {RenderHTML(BlogJson.theBlog.articles[link].headingParagraph)}
                         </div>
+            </section>
+            <section className="blogContent" style={{background:`url(${BlogJson.theBlog.articles[link].image})` , backgroundAttachment:'fixed', backgroundRepeat:'no-repeat', backgroundSize:'cover'}}>
                         <div className="sections contentWrapper">
                             {
                                 Object.keys(BlogJson.theBlog.articles[link].sections).map((sectioName, index) => {
@@ -71,6 +71,16 @@ const SingleBlog = (props) => {
                                     })
                             }
                         </div>
+            </section>
+            <section className="leaveDetails">
+                <h3>בא לך לדבר איתנו על {BlogJson.theBlog.articles[link].title}?</h3>
+                <p>פרטים בקטנה וכבר נחזור אליך!</p>
+                <div className="form">
+                        <input type="text" placeholder="שם מלא.. צריכים להכיר לא?" />
+                        <input type="text" placeholder="מספר נייד, כדי שתוכל לטייל עם הכלב בזמן השיחה :)" />
+                        <input type="email" placeholder="אימייל למקרה שהמספר פלאפון לא נכון" />
+                        <button>יאללה, תחזרו אלי</button>
+                </div>
             </section>
         </div>
     );
