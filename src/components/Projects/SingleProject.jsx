402b82aa50a2fd3dcbc6e36f7f3e3612c86f2e63
MatanSaban <105128031+MatanSaban@users.noreply.github.com>
2022-06-29 17:16:19 +0300

    a

--- src/components/Projects/SingleProject.jsx
+++ src/components/Projects/SingleProject.jsx
@@ -1,9 +1,8 @@
-import { useParams } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import ProjectsJson from "../Projects/Projects.json";
 import "./singleproject.css";
 import { Parallax } from "react-parallax";
 import { useEffect, useRef, useState } from "react";
-
 
 const Container = (props) => (
     <Parallax
@@ -16,11 +15,12 @@ const Container = (props) => (
         )}
         bgImage={props.path}
         strength={400}
-    >
-    </Parallax>
+    ></Parallax>
 );
 
 const SingleProject = (props) => {
+    const [projectsArray, setProjectsArray] = useState([]);
+    const [twoProjects, setTwoprojects] = useState();
 
     const scrollArea = useRef();
     const scrollBoundary = useRef();
@@ -28,34 +28,47 @@ const SingleProject = (props) => {
 
     const [isElementVisible, setIsElementVisible] = useState(false);
 
+    const pageLoad = () => {
+        setTimeout(() => {
+            setTwoprojects(
+                projectsArray.sort(() => 0.5 - Math.random()).slice(0, 2)
+            );
+            window.scrollTo(0, 0);
+        }, 500);
+    };
 
     useEffect(() => {
-        // console.log('myRef', scrollArea.current)
-        // console.log('myRef', scrollBoundary.current)
-        // console.log('myRef', scrolledElement.current)
+        // getMultipleRandom(projectsArray,2)
+        Object.keys(ProjectsJson).map((projectName, index) => {
+            if (ProjectsJson[projectName] !== ProjectsJson[link]) {
+                setProjectsArray((projectsArray) => [
+                    ...projectsArray,
+                    ProjectsJson[projectName],
+                ]);
+            }
+        });
 
         const observer = new IntersectionObserver((entries) => {
             const entry = entries[0];
-            setIsElementVisible(entry.isIntersecting)
-            // console.log('isElementVisible', isElementVisible);
-        })
-        // observer.observe(scrollArea.current);
-        // observer.observe(scrollBoundary.current);
+            setIsElementVisible(entry.isIntersecting);
+        });
         observer.observe(scrolledElement.current);
-        window.scrollTo(0, 0)
-    },[])
+        window.scrollTo(0, 0);
+        // getMultipleRandom(projectsArray, 2);
+    }, []);
 
-
+    useEffect(() => {
+        setTwoprojects(
+            projectsArray.sort(() => 0.5 - Math.random()).slice(0, 2)
+        );
+    }, [projectsArray]);
 
     const { link } = useParams();
 
-    const RenderHTML=(text)=>{
-        const htmlPart=text
-        return(
-          <div dangerouslySetInnerHTML={ {__html: htmlPart} } />
-        )
-    }
-    
+    const RenderHTML = (text) => {
+        const htmlPart = text;
+        return <div dangerouslySetInnerHTML={{ __html: htmlPart }} />;
+    };
 
     return (
         <div id="singleProject" className="projectWrapper">
@@ -68,7 +81,7 @@ const SingleProject = (props) => {
                 <div className="heroCover">
                     <h1>{ProjectsJson[link].name}</h1>
                     <h3>{ProjectsJson[link].whatWeDid}</h3>
-                        { RenderHTML(ProjectsJson[link].description)}
+                    {RenderHTML(ProjectsJson[link].description)}
                     <a
                         style={{
                             background: `${ProjectsJson[link].colors.mainColor}`,
@@ -106,11 +119,10 @@ const SingleProject = (props) => {
                 <Container
                     className="mobileBgSection"
                     path={ProjectsJson[link].mobileBG}
-                >
-                </Container>
-                    <div>
-                        <h5>{ProjectsJson[link].mobileQuote}</h5>
-                    </div>
+                ></Container>
+                <div>
+                    <h5>{ProjectsJson[link].mobileQuote}</h5>
+                </div>
             </section>
             <section
                 className="colorsPassage passage"
@@ -119,7 +131,15 @@ const SingleProject = (props) => {
                     background: `${ProjectsJson[link].colors["mainColor"]}`,
                 }}
             >
-                <div ref={scrolledElement} style={isElementVisible ? {position:'sticky', left:'0', top:'150px'}: {position:'relative'}} className="colorsPallete">
+                <div
+                    ref={scrolledElement}
+                    style={
+                        isElementVisible
+                            ? { position: "sticky", left: "0", top: "150px" }
+                            : { position: "relative" }
+                    }
+                    className="colorsPallete"
+                >
                     {Object.keys(ProjectsJson[link].colors.colorsatts).map(
                         (colorName, index) => {
                             return (
@@ -136,11 +156,13 @@ const SingleProject = (props) => {
                                     }}
                                 >
                                     <p>{colorName}</p>
-                                    <p>{
-                                        ProjectsJson[link].colors.colorsatts[
-                                            colorName
-                                        ].colornumber
-                                    }</p>
+                                    <p>
+                                        {
+                                            ProjectsJson[link].colors
+                                                .colorsatts[colorName]
+                                                .colornumber
+                                        }
+                                    </p>
                                 </div>
                             );
                             // })
@@ -160,7 +182,69 @@ const SingleProject = (props) => {
                 style={{ background: `url(${ProjectsJson[link].laptopBG})` }}
             >
                 <div className="projectSummaryCover">
-                        { RenderHTML(ProjectsJson[link].laptopDescription)}
+                    {RenderHTML(ProjectsJson[link].laptopDescription)}
+                </div>
+            </section>
+            <section className="projectContactForm">
+                <div className="formWrapper">
+                    <div>
+                        <h4>אהבת את האתר של {ProjectsJson[link].name}?</h4>
+                        <p>גם העסק שלך יכול לקבל אתר כזה!</p>
+                    </div>
+                    <div className="lineForm">
+                        <input
+                            type="text"
+                            name="fullname"
+                            id="fullname"
+                            placeholder="שם מלא"
+                        />
+                        <input
+                            type="number"
+                            name="phonenumber"
+                            id="phonenumber"
+                            placeholder="מספר טלפון"
+                        />
+                        <input
+                            type="email"
+                            name="useremail"
+                            id="useremail"
+                            placeholder="אימייל"
+                        />
+                        <input
+                            type="hidden"
+                            name="hidden_site_name"
+                            id="hidden_site_name"
+                            value={ProjectsJson[link].name}
+                        />
+                        <button>שליחה</button>
+                    </div>
+                </div>
+            </section>
+            <section className="nextandprevprojectsWrapper">
+                <h2>פרויקטים נוספים</h2>
+                <div className="nextandprevprojects">
+                    {twoProjects &&
+                        twoProjects.map((i, index) => {
+                            return (
+                                <NavLink
+                                    onClick={() => pageLoad()}
+                                    className="nextProjectItem"
+                                    key={index}
+                                    to={`/projects${i.link}`}
+                                >
+                                    <div
+                                        style={{
+                                            background: `url(${i.backgroundImage})`,
+                                        }}
+                                    >
+                                        <div className="nextProjectItemCover">
+                                            <h3>{i.name}</h3>
+                                            <button>למעבר לפרויקט</button>
+                                        </div>
+                                    </div>
+                                </NavLink>
+                            );
+                        })}
                 </div>
             </section>
         </div>
